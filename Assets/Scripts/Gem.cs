4da36344ce860fa915f48ea1f48217b409a9115a
doslrh15@naver.com <doslrh15@naver.com>
2023-01-01 23:08:23 +0900

    feat : 보석 SFX 추가

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -12,4 +12,10 @@ public class Gem : MonoBehaviour
         rb.AddTorque(new Vector3(Random.Range(2,5), Random.Range(2, 5), Random.Range(2, 5)), ForceMode.Impulse);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        gameObject.GetComponent<AudioSource>().volume = Random.Range(0.6f, 0.8f);
+        gameObject.GetComponent<AudioSource>().pitch = Random.Range(0.8f,1.5f);
+        gameObject.GetComponent<AudioSource>().Play();
+    }
 }
